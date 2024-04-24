11bb324c61a434c3ec70d28a17c4b2bb7e728bc2
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-04-24 17:20:40 +0530

    variables

--- main.go
+++ main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("Hello World")
+	// variables
+
+	var name string = "John Doe"
+	const name2 = "Jane Doe2"
+	var name3 string
+
+	name3 = "Jane Doe3"
+	// name2 = "Jane Doe4" // cannot assign to name2
+	fmt.Println(name,name3,name2);
+
+	// shorthand
+	age := 20 //! cant do outside the function
+	fmt.Println(age)
+
 }
