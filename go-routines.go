aebb9dfeb6e421a5141fb2e35715ff465eb61b89
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-05-28 22:15:23 +0530

    go channels

--- go-routines.go
+++ go-routines.go
@@ -5,50 +5,69 @@ package main
 
 import (
   "fmt"
-  "time"
+  // "time"
 )
 
-func func1(n int){
-  fmt.Println(n)
-  time.Sleep(1 * time.Second)
-}
-
+// func func1(n int){
+//   fmt.Println(n)
+//   time.Sleep(1 * time.Second)
+// }
+//
 func main() {
-  go func1(2)
-
-  go func1(3)
-  go func1(4)
-  go func1(5)
+  // go channels 
   
+  myChannel := make(chan string)
+  
+  go func() {
+    myChannel <- "Hello"
+    myChannel <- "World"
+    close(myChannel)
 
-  // loop {
-  //   time.Sleep(1 * time.Second)
-  // }
+  }()
 
+  // printing both values
 
+  // msgs := <- myChannel
+  for msg := range myChannel {
+    fmt.Println(msg)
+  }
+  // fmt.Println(msgs) 
 
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello3")
-  fmt.Println("Hello4")
-  fmt.Println("Hello5")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello")
-  fmt.Println("Hello222")
+  // go func1(2)
 
+  // go func1(3)
+  // go func1(4)
+  // go func1(5)
+  // 
+  //
+  // // loop {
+  // //   time.Sleep(1 * time.Second)
+  // // }
+  //
+  //
+  //
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello3")
+  // fmt.Println("Hello4")
+  // fmt.Println("Hello5")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello")
+  // fmt.Println("Hello222")
+  //
 }
