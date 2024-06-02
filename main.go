9353a4d1f225e3db43a407d56e84aa491cda1028
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-06-02 16:34:36 +0530

    still piointers

--- main.go
+++ main.go
@@ -43,9 +43,23 @@ type Person struct {
   age int
 }
 
+type rect struct {
+  width int
+  height int
+
+}
+
+func (r *rect) area() int {
+  return r.width * r.height
+}
+
+func (r * rect) peri() int {
+  return 2*r.width + 2*r.height
+}
+
 func newPerson(name string, age int) *Person {
-  p := Person{name:            name , age: 433}
-                     return &p
+  p := Person{name: name , age: 433}
+  return &p
 }
 
 
@@ -66,6 +80,21 @@ func main() {
   fmt.Println(p3);
   fmt.Println(p3.name);
   fmt.Println(p3.age);
+  
+  
+
+  r := rect{height : 10, width :5}
+
+  fmt.Println(r.area())
+  fmt.Println(r.peri())
+
+  r1 := &r
+
+  r1.height = 11
+  fmt.Println(r1.area())
+  fmt.Println(r1.peri())
+
+
   
   
   
