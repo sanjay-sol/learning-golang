574d9b22c806e45c1ce5b0303a461a9ba2a5e55e
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-08-15 16:38:35 +0530

    some concepts of pointers

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func (r * rect) peri() int {
   return 2*r.width + 4*r.height
 }
 
-func newPerson(name string, ageint) *Person {
+func newPerson(name string, age int) *Person {
   p := Person{name: name , age: 433}
   return &p
 }
---
+++ pointers.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func update(name **string ) {
+  **name = "aaa"
+
+
+}
+
+func main()  {
+  num := "sanju"
+  num2 := &num;
+  num3 := &num2;
+  fmt.Println(num);
+  fmt.Println(*num2);
+  fmt.Println(**num3);
+  update(num3);
+  fmt.Printf(num);
+}
