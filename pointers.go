b9c753ca6be1337342ad4cead4f6c829dbaf4299
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-08-18 01:45:14 +0530

    test with pointers

--- pointers.go
+++ pointers.go
@@ -2,36 +2,38 @@ package main
 
 import "fmt"
 
-func update(name **string ) {
-  **name = "aaa"
+// func update(name **string ) {
+//   **name = "aaa"
 
 
-}
+// }
 
-type person struct{
+type Person2 struct{
   name string
   age int
 }
 
-func change(p *person) {
-  p.name = "asdf"
-  p.age = 99
+func (p *Person2) change() *Person2 {
+  p.name = "aaa"
+  p.age = 1111
+  return p
+}
+func (p *Person2) change2() Person2 {
+  p.name = "bbb"
+  p.age = p.age
+  return *p
 }
 
+
+
+
 func main()  {
-  num := "sanju"
-  num2 := &num;
-  num3 := &num2;
-  fmt.Println(num);
-  fmt.Println(*num2);
-  fmt.Println(**num3);
-  update(num3);
-  fmt.Printf(num);
-  person := person{
+  person := Person2{
     name: "sanjay",
     age: 23,
   }
-  change(&person);
-  fmt.Println(person) 
+  p := person.change2();
+  fmt.Println(p); 
+  fmt.Println(person);
   
 }
